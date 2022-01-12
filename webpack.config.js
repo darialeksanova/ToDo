4a8d81c9e2b907ce386dcfb83d48b5cc94bee5b2
Daria Aleksanova <darishubko@gmail.com>
2022-01-12 09:34:05 +0100

    initialize workspace

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  mode: 'development',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  infrastructureLogging: {
+    level: 'none',
+  },
+};
